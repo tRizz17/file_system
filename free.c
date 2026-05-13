95899854e4f011cd92045a32f87e2173a4096ce4
Thomas Rizzo <rizzot@oregonstate.edu>
2026-05-13 13:49:36 -0700

    set_free function implemented

--- free.c
+++ free.c
@@ -3,17 +3,35 @@
 #include <fcntl.h>
 #include <string.h>
 
+// There are two blocks in the file system that represent free/not-free things:
 
-void set_free(unsigned char *block, int num, int set);
+// Block 1: inode map
+// Block 2: Block map
 
+// Set a specific bit to the value in set (0 or 1)
+void set_free(unsigned char *block, int num, int set)
+{
+    int byte_num = num / 8;
+    int bit_num = num % 8;
+    int mask = 1 << bit_num;
+    if (set)
+        block[byte_num] |= mask;
+    else
+        block[byte_num] &= ~mask;
+}
 
-int find_free(unsigned char *block);
+// Find a 0 bit and return its index (i.e. the block number that corresponds to this bit).
+int find_free(unsigned char *block)
+{
+    
+}
 
+// Finds the lowest clear (not-set, zero) bit in a byte
 int find_low_clear_bit(unsigned char x)
 {
     for (int i = 0; i < 8; i++)
         if (!(x & (1 << i)))
             return i;
-    
+
     return -1;
 }
--- mkfs.c
+++ mkfs.c
@@ -5,3 +5,18 @@
 
 
 void mkfs(void);
+
+
+// Write an empty (all zeros) block to the following:
+
+// Block 0: Superblock
+// Block 1: inode map
+// Block 3: inode data block 0
+// Block 4: inode data block 1
+// Block 5: inode data block 2
+// Block 6: inode data block 3
+// Then mark the first 7 blocks as non-free in the block map (block 2).
+
+// You can do this by calling alloc() repeatedly, or (and this is a far more elegant solution), 
+// figure out exactly which byte values need to go in the block map to mark the first 7 blocks as non-free and bwrite() them directly.
+// Hint: 0x7f.
