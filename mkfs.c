731a1536650557304b2d6e067666b78a65e59369
Thomas Rizzo <rizzot@oregonstate.edu>
2026-05-14 22:41:30 -0700

    mkfs.c implemented

--- mkfs.c
+++ mkfs.c
@@ -2,23 +2,19 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-#include "image.c"
+#include "image.h"
+#include "block.h"
+#define RESERVED_BLOCKS_NUM 7
 
-void mkfs(void) {
+void mkfs(void)
+{
     image_open("file_system", 1);
+    for (int i = 0; i < RESERVED_BLOCKS_NUM; i++)
+    {
+        unsigned char block[BLOCK_SIZE] = {0};
+        if (i == 2)
+            block[0] = 0x7f;
+        bwrite(i, block);
+    }
+    image_close();
 }
-
-
-// Write an empty (all zeros) block to the following:
-
-// Block 0: Superblock
-// Block 1: inode map
-// Block 3: inode data block 0
-// Block 4: inode data block 1
-// Block 5: inode data block 2
-// Block 6: inode data block 3
-// Then mark the first 7 blocks as non-free in the block map (block 2).
-
-// You can do this by calling alloc() repeatedly, or (and this is a far more elegant solution), 
-// figure out exactly which byte values need to go in the block map to mark the first 7 blocks as non-free and bwrite() them directly.
-// Hint: 0x7f.
