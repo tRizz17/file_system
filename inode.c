b880d03b5fc41aad3fb537a1589d2f7994f98d07
Thomas Rizzo <rizzot@oregonstate.edu>
2026-05-18 15:05:04 -0700

    incore_find() and incore_free_all() implemented

--- inode.c
+++ inode.c
@@ -44,8 +44,20 @@ struct inode *incore_find_free(void)
 
 struct inode *incore_find(unsigned int inode_num)
 {
+    for (int i = 0; i < MAX_SYS_OPEN_FILES; i++)
+    {
+        if (incore[i].ref_count != 0 && (incore[i].inode_num == inode_num))
+        {
+            return (incore + i);
+        }
+    }
+    return NULL;
 }
 
 void incore_free_all(void)
 {
+    for (int i = 0; i < MAX_SYS_OPEN_FILES; i++)
+    {
+        incore[i].ref_count = 0;
+    }
 }
